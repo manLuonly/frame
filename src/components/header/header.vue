c716e1b46c3ad2f0dd7f7d1d52242d03578bfd02
LuGuangZong <1454118276@qq.com>
2019-06-19 23:10:17 +0800

    'init'

---
+++ src/components/header/header.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+
+  data () {
+    return {
+    }
+  },
+
+  methods: {}
+}
+</script>
+
+<style lang='scss' scoped>
+
+</style>
