2188068d6890e80cbd72f7111555f6b232133538
LuGuangZong <1454118276@qq.com>
2019-06-20 00:26:58 +0800

    'init'

---
+++ src/api/index.js
@@ -0,0 +1,58 @@
+import axios from 'axios'
+import router from '@/router'
+
+window.Promise = Promise
+
+// axios 配置
+//正式环境：0， 测试环境：1
+let test_env = 0;
+// axios 配置
+axios.defaults.timeout = 100000;
+if (test_env === 1) {
+    // 测试环境
+    axios.defaults.baseURL = '';
+} else {
+    // 正式环境
+    axios.defaults.baseURL = ''; //sever1
+}
+// http request 拦截器
+axios.interceptors.request.use(
+    config => {
+        // if (localStorage.token) {
+        //   config.headers.Authorization = `token ${localStorage.token}`;
+        // }
+        config.headers['X-Token'] = `fd971b3eef2d460083b12dfdc077ff5f0`;
+        return config;
+    },
+    err => {
+        return Promise.reject(err);
+    });
+
+
+// http response 拦截器
+axios.interceptors.response.use(
+    response => {
+        return response;
+    },
+    error => {
+        if (error.response) {
+            switch (error.response.status) {
+                case 401:
+                    // 返回 401 清除token信息并跳转到登录页面
+                    localStorage.removeItem('token')
+                    localStorage.removeItem('username')
+                        // router.replace({
+                        //   path: '/login',
+                        //   query: {redirect: router.currentRoute.fullPath}
+                        // })
+            }
+        }
+        if (error.response) {
+            return Promise.reject(error.response.data) // 返回接口返回的错误信息
+        } else {
+            return Promise.reject(error)
+        }
+    });
+
+
+export default axios;
--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './router'
 import store from './store'
 
+
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+
+Vue.use(VueAxios, axios);
 Vue.config.productionTip = false
 
 new Vue({
