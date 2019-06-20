2a21dec210401d9446065003aac3211394f18230
LuGuangZong <1454118276@qq.com>
2019-06-20 21:14:36 +0800

    'update'

--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,24 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import getters from './getters'
-import mutations from './mutations'
-import actions from './action'
-
+import search_module from './search/index'
 
 Vue.use(Vuex)
 
 const state = {
 
 }
+const getters = {}
+
+const mutations = {}
+
+const actions = {}
 
 export default new Vuex.Store({
     state,
     getters,
     mutations,
     actions,
+    modules: {
+        search: search_module
+    }
 })
