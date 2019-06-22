e6fe41cd31a5491a78e5da5b969c2d91c5c2ac69
buhtib <2418531916@qq.com>
2019-06-23 00:03:39 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-import './router'
+import router from './router'
 import store from './store'
-require('./public')
+import ('./public')
 /**
  * 挂载公共组件
  */
@@ -19,5 +19,6 @@ Vue.config.productionTip = false
 
 new Vue({
     store,
+    router,
     render: h => h(App)
 }).$mount('#app')
